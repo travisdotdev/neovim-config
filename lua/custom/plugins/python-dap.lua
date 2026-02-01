9da63951976cfd1ab1579a97e681b1943d33340c
travisdotdev <travis4yusuf@gmail.com>
2026-02-01 14:01:29 +0000

    DAP works with Unittest

--- lua/custom/plugins/python-dap.lua
+++ lua/custom/plugins/python-dap.lua
@@ -1,22 +1,50 @@
 return {
-  'mfussenegger/nvim-dap-python',
-  ft = 'python',
-  dependencies = {
-    'mfussenegger/nvim-dap',
-  },
-  config = function()
-    -- Update this path to where debugpy is installed
-    -- Mason installs it at:
-    local debugpy_path = vim.fn.stdpath 'data' .. '/mason/packages/debugpy/venv/bin/python'
-    require('dap-python').setup(debugpy_path)
+    'mfussenegger/nvim-dap-python',
+    ft = 'python',
+    dependencies = {
+        'mfussenegger/nvim-dap',
+    },
+    config = function()
+        -- Hardcode your venv path for now
+        local venv_python = vim.fn.getcwd() .. '/venv/bin/python'
 
-    -- Optional: Add key mappings for Python debugging
-    vim.keymap.set('n', '<leader>dpr', function()
-      require('dap-python').test_method()
-    end, { desc = 'Debug Python: Run test method' })
+        -- Check if venv exists, otherwise fallback to Mason
+        local python_path = vim.fn.executable(venv_python) == 1 and venv_python or
+        vim.fn.stdpath 'data' .. '/mason/packages/debugpy/venv/bin/python'
 
-    vim.keymap.set('n', '<leader>dpc', function()
-      require('dap-python').test_class()
-    end, { desc = 'Debug Python: Run test class' })
-  end,
+        require('dap-python').setup(python_path)
+
+        local dap = require 'dap'
+
+        table.insert(dap.configurations.python, {
+            type = 'python',
+            request = 'launch',
+            name = 'Debug All Tests (unittest discover)',
+            module = 'unittest',
+            args = { 'discover', '-s', 'src', '-v' },
+            console = 'integratedTerminal',
+            justMyCode = true,
+            pythonPath = python_path, -- Use the same Python path
+        })
+
+        vim.keymap.set('n', '<leader>dt', function()
+            -- Print which Python we're using (for debugging)
+            print('Using Python: ' .. python_path)
+
+            for _, config in ipairs(dap.configurations.python) do
+                if config.name == 'Debug All Tests (unittest discover)' then
+                    dap.run(config)
+                    break
+                end
+            end
+        end, { desc = 'Debug All Tests (test.sh equivalent)' })
+
+        vim.keymap.set('n', '<leader>dpr', function()
+            require('dap-python').test_method()
+        end, { desc = 'Debug Python: Run test method' })
+
+        vim.keymap.set('n', '<leader>dpc', function()
+            require('dap-python').test_class()
+        end, { desc = 'Debug Python: Run test class' })
+    end,
 }
