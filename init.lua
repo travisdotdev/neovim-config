7e5fb78dc5c980f173fb794a065627dad1ed37c6
travisdotdev <travis4yusuf@gmail.com>
2026-03-26 11:26:04 +0000

    update

--- init.lua
+++ init.lua
@@ -744,9 +744,6 @@ require('lazy').setup({
   { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   rocks = {
     enabled = false,
