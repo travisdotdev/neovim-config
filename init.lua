0cad0832ccd357b252a527f369c039c75646332a
travisdotdev <travis4yusuf@gmail.com>
2026-03-28 11:09:52 +0000

    Update

--- init.lua
+++ init.lua
@@ -274,7 +274,6 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
       'mason-org/mason-lspconfig.nvim',
